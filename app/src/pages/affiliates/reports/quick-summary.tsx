0d9f5998e37e7bb7d5b6c29606463d3a95881b47
DripMicro <whappydrip@gmail.com>
2023-02-09 12:23:48 +0300

    quick report get summary added

--- app/src/pages/affiliates/reports/quick-summary.tsx
+++ app/src/pages/affiliates/reports/quick-summary.tsx
@@ -6,14 +6,18 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import {useState} from 'react'
 import { RangeDatepicker } from "chakra-dayzed-datepicker";
+import { Select } from '@chakra-ui/react'
 
 const Page: NextPage = () => {
+  const [displayType, setDisplayType] = useState("");
   const [selectedDates, setSelectedDates] = useState<Date[]>([new Date(), new Date()]);
-  const { data } = api.affiliates.getQuickReportSummary.useQuery({from:selectedDates[0],to:selectedDates[1]})
+  const { data } = api.affiliates.getQuickReportSummary.useQuery({from:selectedDates[0],to:selectedDates[1], display: displayType})
+  const {data: merchants} = api.affiliates.getAllMerchants.useQuery();
+
 
   
   console.log("data ----->", data)
-  console.log("selected dates ------>",selectedDates)
+  console.log("merchants ----->",merchants);
   
 
   return (
@@ -30,6 +34,17 @@ const Page: NextPage = () => {
         selectedDates={selectedDates}
         onDateChange={setSelectedDates}
       />
+      <Select placeholder='Select option' onChange={(event) => setDisplayType(event.target.value)}>
+      <option value='monthly'>monthly</option>
+      <option value='weekly'>weekly</option>
+      <option value='daily'>daily</option>
+    </Select>
+    <Select placeholder='Select option' onChange={(event) => setDisplayType(event.target.value)}>
+      {merchants?.map(merchant => {
+        return <option value={merchant.name}>{merchant.name}</option>
+
+      })}
+    </Select>
       </Flex>
       </Flex>
       </main>
--- app/src/server/api/routers/affiliates/reports.ts
+++ app/src/server/api/routers/affiliates/reports.ts
@@ -2,9 +2,9 @@ import {  z } from "zod";
 
 
 import {publicProcedure} from '../../trpc'
-import { countBy, map, sortBy, uniq, uniqBy, uniqWith } from "rambda";
-import type { dashboard, merchants_creative_type, Prisma } from "@prisma/client";
-import { affiliate_id, merchant_id } from "./const";
+import { groupBy } from "rambda";
+import type { dashboard } from "@prisma/client";
+import {  merchant_id } from "./const";
 import { SelectSchema } from "../../../db-schema-utils";
 import {
   addFreeTextSearchJSFilter,
@@ -15,77 +15,48 @@ import moment from "moment-mini";
 
 export const getQuickReportSummary = publicProcedure.input(z.object({
     from:z.date().optional(),
-    to: z.date().optional()
-})).query(async ({ctx,input: {from, to}}) => {
-    console.log(from,to)
-    let data = await ctx.prisma.dashboard.findMany({
+    to: z.date().optional(),
+    display:z.string().optional()
+})).query(async ({ctx,input: {from, to,display}}) => {
+    console.log(display)
+
+    let data:any = await ctx.prisma.dashboard.groupBy({
+        by:['merchant_id','Date'],
         where: {
+            ...(merchant_id ?{merchant_id}: {}),
             Date:{
-                gt:from,
-                lte:to
+                gte:from,
+                lt:to
             }
         },
-        take:10
-    })
-    const Clicks = data.reduce((acc:any,i:dashboard) => acc + i.Clicks,0);
-    const Impressions = data.reduce((acc:any,i:dashboard) => acc + i.Impressions,0);
-    const Install = data.reduce((acc:any,i:dashboard) => acc + i.Install,0);
-    const Leads = data.reduce((acc:any,i:dashboard) => acc + i.Leads,0);
-    const Demo = data.reduce((acc:any,i:dashboard) => acc + i.Demo,0);
-    const RealAccount = data.reduce((acc:any,i:dashboard) => acc + i.RealAccount,0);
-    const FTD = data.reduce((acc:any,i:dashboard) => acc + i.FTD,0);
-    const FTDAmount = data.reduce((acc:any,i:dashboard) => acc + i.FTDAmount,0);
-    const RawFTD = data.reduce((acc:any,i:dashboard) => acc + i.RawFTD,0);
-    const RawFTDAmount = data.reduce((acc:any,i:dashboard) => acc + i.RawFTDAmount,0);
-    const Deposits = data.reduce((acc:any,i:dashboard) => acc + i.Deposits,0);
-    const DepositsAmount = data.reduce((acc:any,i:dashboard) => acc + i.DepositsAmount,0);
-    const Bonus = data.reduce((acc:any,i:dashboard) => acc + i.Bonus,0);
-    const Withdrawal = data.reduce((acc:any,i:dashboard) => acc + i.Withdrawal,0);
-    const ChargeBack = data.reduce((acc:any,i:dashboard) => acc + i.ChargeBack,0);
-    const NetDeposit = data.reduce((acc:any,i:dashboard) => acc + i.NetDeposit,0);
-    const PNL = data.reduce((acc:any,i:dashboard) => acc + i.PNL,0);
-    const Volume = data.reduce((acc:any,i:dashboard) => acc + i.Volume,0);
-    const ActiveTrader = data.reduce((acc:any,i:dashboard) => acc + i.ActiveTrader,0);
-    const Commission = data.reduce((acc:any,i:dashboard) => acc + i.Commission,0);
-    const PendingDepositsAmount = data.reduce((acc:any,i:dashboard) => acc + i.PendingDepositsAmount,0);
-    const PendingDeposits = data.reduce((acc:any,i:dashboard) => acc + i.PendingDeposits,0);
-    
-    let Week,Month,Year;
-
-    data.map(item => {
-        Week = moment(item.Date).week()
-        Month = moment(item.Date).month()
-        Year = moment(item.Date).year()
+        _sum:{
+            Clicks:true,
+            Impressions:true,
+            Install:true,
+            Leads:true,
+            Demo:true,
+            RealAccount:true,
+            FTD:true,
+            FTDAmount:true,
+            Deposits:true,
+            DepositsAmount:true,
+            Bonus:true,
+            RawFTD:true,
+            RawFTDAmount:true,
+            Withdrawal:true,
+            ChargeBack:true,
+            NetDeposit:true,
+            PNL:true,
+            ActiveTrader:true,
+            Commission:true,
+            PendingDeposits:true,
+            PendingDepositsAmount:true
+        },
     })
 
-    return {
-        ...data,
-        Clicks,
-        Impressions,
-        Install,
-        Leads,
-        Demo,
-        RealAccount,
-        FTD,
-        FTDAmount,
-        RawFTD,
-        RawFTDAmount,
-        Deposits,
-        DepositsAmount,
-        Bonus,
-        Withdrawal,
-        ChargeBack,
-        NetDeposit,
-        PNL,
-        Volume,
-        ActiveTrader,
-        Commission,
-        PendingDepositsAmount,
-        PendingDeposits,
-        Week,
-        Month,
-        Year
-    };
+    console.log("Data -------> ",data)
+
+    return data;
 })
 
 export const getDataInstall = publicProcedure.query(async ({ ctx }) => {
@@ -104,11 +75,8 @@ export const getDataInstall = publicProcedure.query(async ({ ctx }) => {
 
 
 export const getAllMerchants = publicProcedure.query(async ({ctx}) => {
-    const data = await ctx.prisma.merchants.findMany({
-        where: {
-            valid:1
-        }
+    const merchants = await ctx.prisma.merchants.findMany({
     })
 
-    return data;
+    return merchants;
 })
