3dddefb5406c9a901819c8a3199c74567c4cf977
DripMicro <whappydrip@gmail.com>
2023-02-14 20:21:24 +0200

    WIP file upload

--- app/src/env.mjs
+++ app/src/env.mjs
@@ -20,8 +20,11 @@ const server = z.object({
         process.env.VERCEL ? z.string().min(1) : z.string().url(),
     ),
     // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
+    // DISCORD_CLIENT_ID: z.string(),
+    // DISCORD_CLIENT_SECRET: z.string(),
+
+    LEGACY_PHP_API_URL: z.string(),
+    LEGACY_PHP_ACCESS_TOKEN: z.string(),
 });
 
 /**
@@ -43,6 +46,9 @@ const processEnv = {
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+
+    LEGACY_PHP_API_URL: process.env.LEGACY_PHP_API_URL,
+    LEGACY_PHP_ACCESS_TOKEN: process.env.LEGACY_PHP_ACCESS_TOKEN,
     // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
