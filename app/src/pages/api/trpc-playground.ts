bc0d2e16782d5539abaf2a2c0fa27bc51c4141fa
DripMicro <whappydrip@gmail.com>
2023-02-16 11:02:17 +0200

    fix trpc-playgoround import

--- app/src/pages/api/trpc-playground.ts
+++ app/src/pages/api/trpc-playground.ts
@@ -1,22 +1,22 @@
 // pages/api/trpc-playground.ts
-import type { NextApiHandler } from 'next'
-import { appRouter } from 'app/src/server/api/root'
-import { nextHandler } from 'trpc-playground/handlers/next'
+import type { NextApiHandler } from "next";
+import { appRouter } from "../../server/api/root";
+import { nextHandler } from "trpc-playground/handlers/next";
 
 const setupHandler = nextHandler({
-    router: appRouter,
-    // tRPC api path, pages/api/trpc/[trpc].ts in this case
-    trpcApiEndpoint: '/api/trpc',
-    playgroundEndpoint: '/api/trpc-playground',
-    // uncomment this if you're using superjson
-    request: {
-      superjson: true,
-    },
-})
+  router: appRouter,
+  // tRPC api path, pages/api/trpc/[trpc].ts in this case
+  trpcApiEndpoint: "/api/trpc",
+  playgroundEndpoint: "/api/trpc-playground",
+  // uncomment this if you're using superjson
+  request: {
+    superjson: true,
+  },
+});
 
 const handler: NextApiHandler = async (req, res) => {
-    const playgroundHandler = await setupHandler
-    await playgroundHandler(req, res)
-}
+  const playgroundHandler = await setupHandler;
+  await playgroundHandler(req, res);
+};
 
-export default handler
+export default handler;
