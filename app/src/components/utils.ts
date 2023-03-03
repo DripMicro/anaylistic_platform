69ddee1bf23307d6f7f7859ddf1daae7c38f9e6c
DripMicro <whappydrip@gmail.com>
2023-03-03 08:00:23 +0200

    try to solve reading env variable

--- app/src/components/utils.ts
+++ app/src/components/utils.ts
@@ -1,5 +1,5 @@
 import { env } from "../env.mjs";
 
-export const storageBasePath = String(env.LEGACY_PHP_URL);
+export const storageBasePath = String(env.NEXT_PUBLIC_LEGACY_PHP_URL);
 
 export const serverStoragePath = (path: string) => `${storageBasePath}/${path}`;
--- app/src/env.mjs
+++ app/src/env.mjs
@@ -23,7 +23,6 @@ const server = z.object({
   // DISCORD_CLIENT_ID: z.string(),
   // DISCORD_CLIENT_SECRET: z.string(),
 
-  LEGACY_PHP_URL: z.string().url(),
   LEGACY_PHP_API_URL: z.string(),
   LEGACY_PHP_ACCESS_TOKEN: z.string(),
 });
@@ -34,7 +33,7 @@ const server = z.object({
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+  NEXT_PUBLIC_LEGACY_PHP_URL: z.string().url(),
 });
 
 /**
@@ -48,7 +47,7 @@ const processEnv = {
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 
-  LEGACY_PHP_URL: process.env.LEGACY_PHP_URL,
+  NEXT_PUBLIC_LEGACY_PHP_URL: process.env.NEXT_PUBLIC_LEGACY_PHP_URL,
   LEGACY_PHP_API_URL: process.env.LEGACY_PHP_API_URL,
   LEGACY_PHP_ACCESS_TOKEN: process.env.LEGACY_PHP_ACCESS_TOKEN,
   // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
