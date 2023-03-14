c6524e98c79978588509188a46ea9809cf0f6d05
DripMicro <whappydrip@gmail.com>
2023-03-14 22:53:19 +0200

    sign in
    UI design

--- app/src/components/common/forms/Form.tsx
+++ app/src/components/common/forms/Form.tsx
@@ -1,11 +1,13 @@
 import { createTsForm } from "../../libs/react-ts-form";
+import { useState } from "react";
 import { mapping } from "./mapping";
 import type { FormEvent } from "react";
 import React from "react";
-import { Button, Stack } from "@chakra-ui/react";
+import { Button, Stack, Link } from "@chakra-ui/react";
 import type { GridProps } from "@chakra-ui/layout/dist/grid";
 import { FormLayout } from "./FormLayout";
 import { useSubmitAction } from "./useSubmitAction";
+import NextLink from "next/link";
 
 export interface CommonFormProps {
   onSubmit: (values: unknown) => Promise<void>;
@@ -29,6 +31,8 @@ const CommonForm = ({
     submitNotification,
   });
 
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <form
       onSubmit={(e: FormEvent) => {
@@ -36,10 +40,12 @@ const CommonForm = ({
         void handleSubmit(e);
       }}
       noValidate
+      className="w-full md:w-[33rem]"
     >
       <Stack>
-        <FormLayout grid={grid}>{children}</FormLayout>
-        <Button
+        {/* <FormLayout grid={grid}>{children}</FormLayout> */}
+        {/* <Button
+          className="w-full bg-[#2262C6]"
           minW={36}
           type="submit"
           variant="solid"
@@ -47,7 +53,86 @@ const CommonForm = ({
           alignSelf="start"
         >
           {submitButtonText ? submitButtonText : "SAVE"}
-        </Button>
+        </Button> */}
+
+        <div className="text-4xl text-black flex flex-col items-center mt-20 mb-16 md:mt-28 md:mb-24">
+          Login to Your
+          <div className="flex items-center">
+            <img className="mt-2" src="/img/logo.png" width="109"/>
+            <span className="ml-3 text-black">account</span>
+          </div>
+        </div>
+
+        <div className="pb-4">
+          <label className="block text-gray-600 text-base mb-1.5 ml-2.5 font-medium">
+            Username
+          </label>
+          <input className="border rounded-md w-full py-4 px-3 text-gray-700 font-normal text-base" id="username" type="text" placeholder="Type Here..."/>
+        </div>
+
+        <div className="pb-2.5 relative">
+          <label className="block text-gray-600 text-base mb-1.5 ml-2.5 font-medium">
+            Password
+          </label>
+          <input className="border rounded-md w-full py-4 px-3 text-gray-700 font-normal text-base" id="username" type={showPassword?'text':'password'} placeholder="Type Here..."/>
+          <label
+            onClick={()=>setShowPassword(!showPassword)}
+            className="absolute mt-4 right-4 cursor-pointer " htmlFor="toggle">
+              {showPassword?
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z">
+                  </path>
+                </svg>
+              :
+                <svg className="w-6 h-6 opacity-60" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21">
+                  </path>
+                </svg>
+              }
+          </label>
+
+        </div>
+
+        <div className="grid justify-items-end pb-5">
+          <Link as={NextLink} href="/affiliates/lost-password">
+            <span className="text-sm text-black">
+              Forgot your Username or Password?
+            </span>
+          </Link>
+        </div>
+        
+        <button className="w-full bg-[#2262C6] text-white py-3 font-semibold rounded-md">
+          {submitButtonText ? submitButtonText : "SAVE"}
+        </button>
+        
+        <div className="text-center pt-6 md:pt-14 text-sm md:text-xl">
+          Don’t have an account yet?&nbsp;
+          <Link as={NextLink} href="/affiliates/signup">
+            <span className="text-[#2262C6] text-sm md:text-xl font-bold">
+              Sign Up
+            </span>
+          </Link>
+        </div>
+        
+        <div className="flex flex-col items-center pt-3 md:pt-8">
+          <div className="h-px bg-[#D7D7D7] md:w-[32rem] w-72"></div>
+        </div>
+
+        <div className="text-center pt-3 md:pt-8 text-sm md:text-xl">
+          Are you an admin?&nbsp;
+          <Link as={NextLink} href="/affiliates/signup">
+            <span className="text-[#2262C6] text-sm md:text-xl font-bold">
+              Sign In here
+            </span>
+          </Link>
+        </div>
+        
       </Stack>
     </form>
   );
--- app/src/pages/affiliates/signin.tsx
+++ app/src/pages/affiliates/signin.tsx
@@ -1,6 +1,6 @@
-import styles from "./../index.module.css";
 import { type NextPage } from "next";
 import Head from "next/head";
+import {AuthenticationFooter} from "../../components/common/footer/AuthenticationFooter"; 
 
 import { FormSignin } from "../../components/affiliates/account/FormSignin";
 const Page: NextPage = () => {
@@ -11,8 +11,9 @@ const Page: NextPage = () => {
         <meta name="description" content="Affiliates Creative Materials" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className="flex flex-col items-center min-h-screen px-5">
         <FormSignin />
+        <AuthenticationFooter/>
       </main>
     </>
   );
